80e6177b03c430f82f15829baca9ddccbf3397bf
himaja_a <himaja_a@hcl.com>
2018-06-17 23:26:45 +0530

    Modified images and added packages

---
+++ web/js/config.js
@@ -0,0 +1,16 @@
+(function($, document, window){
+properties = {
+  // This can have nested stuff, arrays, etc.
+  companyName: 'Sai Lalitha Boat Tourism ',
+  tagLine: 'Adds new memories to your dairy',
+  copyRight :'Copyright 2018 Satya Sri Punnami Tours & Travels, Designed by Himaja Nakkina, All right reserved.',
+  address:' ABC, DEF',
+  phone:' +91 1234567890',
+  email:' abc@gmail.com'
+}
+
+	$(window).ready(function(){
+
+	});
+
+})(jQuery, document, window);
